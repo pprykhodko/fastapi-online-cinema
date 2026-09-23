29b217c2433c5e9e7932e16de76a5ee501b4ed97
Petro Prykhodko <petro.prykhodko2004@gmail.com>
2026-09-23 22:32:05 +0200

    refactor: change comments endpoints place in Swagger list

--- src/main.py
+++ src/main.py
@@ -28,19 +28,15 @@ app = FastAPI(
         "An API for browsing movies, managing user accounts, "
         "shopping carts, orders, and payments."
     ),
-    lifespan=lifespan,
+    lifespan=lifespan
 )
 
 api_version_prefix = "/api/v1"
 
-app.include_router(
-    comments_router, prefix=api_version_prefix, tags=["comments"],
-)
-
 
 @app.exception_handler(RequestValidationError)
 async def request_validation_error_handler(
-    _request: Request, error: RequestValidationError,
+        _request: Request, error: RequestValidationError
 ) -> JSONResponse:
     details = [
         {"type": item["type"], "loc": item["loc"], "msg": item["msg"]}
@@ -49,66 +45,72 @@ async def request_validation_error_handler(
 
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content={"detail": details},
+        content={"detail": details}
     )
 
 
 app.include_router(
     accounts_router,
     prefix=f"{api_version_prefix}/accounts",
-    tags=["accounts"],
+    tags=["accounts"]
 )
 
 app.include_router(
     profiles_router,
     prefix=f"{api_version_prefix}/profiles",
-    tags=["profiles"],
+    tags=["profiles"]
 )
 
 app.include_router(
     movies_router,
     prefix=f"{api_version_prefix}/movies",
-    tags=["movies"],
+    tags=["movies"]
 )
 
 app.include_router(
     genres_router,
     prefix=f"{api_version_prefix}/genres",
-    tags=["genres"],
+    tags=["genres"]
 )
 
 app.include_router(
     stars_router,
     prefix=f"{api_version_prefix}/stars",
-    tags=["stars"],
+    tags=["stars"]
 )
 
 app.include_router(
     directors_router,
     prefix=f"{api_version_prefix}/directors",
-    tags=["directors"],
+    tags=["directors"]
 )
 
 app.include_router(
     certifications_router,
     prefix=f"{api_version_prefix}/certifications",
-    tags=["certifications"],
+    tags=["certifications"]
 )
 
 app.include_router(
     favorites_router,
     prefix=f"{api_version_prefix}/favorites",
-    tags=["favorites"],
+    tags=["favorites"]
 )
 
 app.include_router(
     reactions_router,
     prefix=f"{api_version_prefix}/movies",
-    tags=["reactions"],
+    tags=["reactions"]
 )
 
 app.include_router(
     ratings_router,
     prefix=f"{api_version_prefix}/movies",
-    tags=["ratings"],
+    tags=["ratings"]
+)
+
+app.include_router(
+    comments_router,
+    prefix=api_version_prefix,
+    tags=["comments"]
 )
